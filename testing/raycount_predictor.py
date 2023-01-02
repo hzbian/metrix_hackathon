2bea990df53e4808097b29580c530d128a07ff77
David Meier <meier.david@uni-kassel.de>
2023-01-02 23:14:20 +0100

    Add initial version of raycount predictor

---
+++ testing/raycount_predictor.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+sys.path.insert(0, '../')
+
+from tqdm import tqdm
+
+from ray_tools.simulation.torch_datasets import RayDataset
+
+import torch
+from torch.utils.data import DataLoader
+
+# Important fix to make custom collate_fn work
+# https://forums.fast.ai/t/runtimeerror-received-0-items-of-ancdata/48935
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+h5_path = os.path.join('../datasets/metrix_simulation/ray_enhance')
+
+h5_files = [os.path.join(h5_path, file) for file in os.listdir(h5_path) if file.endswith('.h5')]
+
+dataset = RayDataset(h5_files=h5_files,
+                     sub_groups=['1e6/params',
+                                 '1e6/ray_output/ImagePlane/n_rays'])
+
+batch_size = 100
+data_loader = DataLoader(dataset,
+                         shuffle=True,
+                         batch_size=batch_size,
+                         num_workers=0)
+
+for idx, item in tqdm(enumerate(data_loader)):
+    print(idx, item)
+    pass
