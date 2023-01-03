baaad593d9e30610b0b4acd61510b078474f31f8
Martin Genzel <martingenzel@googlemail.com>
2023-01-03 15:26:12 +0100

    improved RayDataset

--- ray_tools/simulation/torch_datasets.py
+++ ray_tools/simulation/torch_datasets.py
@@ -1,4 +1,6 @@
-from typing import List, Callable
+from typing import List, Callable, Any, Dict
+
+from tqdm import tqdm
 
 import h5py
 
@@ -11,10 +13,12 @@ class RayDataset(Dataset):
     def __init__(self,
                  h5_files: List[str],
                  sub_groups: List[str],
+                 nested_groups: bool = False,
                  transform: Callable = None):
 
         self.h5_files = h5_files
         self.sub_groups = sub_groups
+        self.nested_groups = nested_groups
         # open all h5_files
         self.h5_files_obj = [h5py.File(f, "r", swmr=True, libver='latest') for f in self.h5_files]
 
@@ -41,24 +45,34 @@ class RayDataset(Dataset):
         for h5_file_obj in self.h5_files_obj:
             h5_file_obj.close()
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx, sub_groups: List[str] = None, nested_groups: bool = None) -> Dict[str, Any]:
+        sub_groups = sub_groups if sub_groups is not None else self.sub_groups
+        nested_groups = nested_groups if nested_groups is not None else self.nested_groups
+
         idx_h5, idx_sample = self.get_identifier[idx]
-
         sample_grp: h5py.Group = self.h5_files_obj[idx_h5][idx_sample]
+
         data = {}
-        for grp in self.sub_groups:
-            grp_split = grp.split(sep='/')
-            sub_dict = data
-            for key in grp_split:
-                if key not in sub_dict:
-                    sub_dict[key] = {}
-                sub_dict = sub_dict[key]
+        for grp in sub_groups:
+            if nested_groups:
+                grp_split = grp.split(sep='/')
+                sub_dict = data
+                for key in grp_split[:-1]:
+                    if key not in sub_dict:
+                        sub_dict[key] = {}
+                    sub_dict = sub_dict[key]
+                sub_dict[grp_split[-1]] = h5_to_dict(sample_grp[grp])
+            else:
+                data[grp] = h5_to_dict(sample_grp[grp])
 
-            sub_dict.update(h5_to_dict(sample_grp[grp]))
-
-        if self.transform is not None:
-            data = self.transform(data)
-        return data
+        return self.transform(data) if self.transform else data
 
     def __len__(self):
         return self._n_samples_total
+
+
+def extract_field(dataset: RayDataset, field: str) -> List[Any]:
+    data = len(dataset) * [None]
+    for idx in tqdm(range(len(dataset))):
+        data[idx] = list(dataset.__getitem__(idx, sub_groups=[field], nested_groups=False).values())[0]
+    return data
