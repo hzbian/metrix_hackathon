Fix wrong index in transform

ray_nn/data/transform.py
@@ -18,6 +18,6 @@ class Select(torch.nn.Module):
                 # TODO: might need to be extended for recursion
                 new_element = torch.hstack([torch.tensor(i) for i in new_element.values()]).float()
             else:
-                new_element = torch.tensor(batch[key]).float().unsqueeze(-2)
+                new_element = torch.tensor(batch[key]).float().unsqueeze(-1)
             outputs.append(new_element)
         return tuple(outputs)