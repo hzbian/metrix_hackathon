eca7961703023cf5ad73295045030a23d7f928d6
David Meier <meier.david@uni-kassel.de>
2023-06-19 18:56:06 +0200

    Make cuda optional

--- sub_projects/ray_optimization/losses.py
+++ sub_projects/ray_optimization/losses.py
@@ -8,12 +8,14 @@ sinkhorn_function = SinkhornLoss(normalize_weights='weights1', p=1, backend='onl
 
 
 def sinkhorn_loss(y: torch.Tensor, y_hat: torch.Tensor):
-    y = y.cuda()
+    if torch.cuda.is_available():
+        y = y.cuda()
 
     if y.shape[1] == 0 or y.shape[1] == 1:
         y = torch.ones((y.shape[0], 2, 2), device=y.device, dtype=y.dtype) * -2
 
-    y_hat = y_hat.cuda()
+    if torch.cuda.is_available():
+        y_hat = y_hat.cuda()
     if y_hat.shape[1] == 0 or y_hat.shape[1] == 1:
         y_hat = torch.ones((y_hat.shape[0], 2, 2), device=y_hat.device, dtype=y_hat.dtype) * -1
     loss = sinkhorn_function(y.contiguous(), y_hat.contiguous(), torch.ones_like(y[..., 1]),
@@ -24,7 +26,8 @@ def sinkhorn_loss(y: torch.Tensor, y_hat: torch.Tensor):
 
 
 def multi_objective_loss(y: torch.Tensor, y_hat: torch.Tensor):
-    y = y.cuda()
-    y_hat = y_hat.cuda()
+    if torch.cuda.is_available():
+        y = y.cuda()
+        y_hat = y_hat.cuda()
     ray_count_loss = (y.shape[1] - y_hat.shape[1]) ** 2 / 2
     return sinkhorn_loss(y, y_hat), ray_count_loss
