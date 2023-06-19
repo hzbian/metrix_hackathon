24e02e23d352f67df621c462717f3f7515f88350
David Meier <meier.david@uni-kassel.de>
2023-06-19 18:58:40 +0200

    Add config for gauss engine

---
+++ sub_projects/ray_optimization/config/config_gauss.py
@@ -0,0 +1,52 @@
+from optuna.samplers import TPESampler
+
+from ray_tools.base.engine import GaussEngine
+from ray_tools.base.parameter import RayParameterContainer, NumericalParameter, RandomParameter
+from ray_tools.base.transform import MultiLayer
+from ray_tools.base.utils import RandomGenerator
+
+# objective
+REAL_DATA_DIR = None
+EXPORTED_PLANE = "ImagePlane"
+MAX_DEVIATION = 0.3
+N_RAYS = ['1e4']
+Z_LAYERS = [-15, -10, -5, 0, 5, 10, 15, 20, 25, 30]
+TRANSFORMS = MultiLayer(Z_LAYERS, copy_directions=False)
+NUM_BEAMLINE_PARAM_SAMPLES = 22
+RG = RandomGenerator(seed=42)
+
+PARAM_FUNC = lambda: RayParameterContainer([
+    ("number_rays", NumericalParameter(value=1e4)),
+    ("x_dir", NumericalParameter(value=1.)),
+    ("y_dir", NumericalParameter(value=1.)),
+    ("z_dir", NumericalParameter(value=1.)),
+    ("direction_spread", RandomParameter(value_lims=(0., 0.1), rg=RG)),
+    ("x_mean", RandomParameter(value_lims=(-2., 2.), rg=RG)),
+    ("y_mean", RandomParameter(value_lims=(-2., 2.), rg=RG)),
+    ("x_var", RandomParameter(value_lims=(0.67, 2.), rg=RG)),
+    ("y_var", RandomParameter(value_lims=(0.67, 2.), rg=RG)),
+])
+
+FIXED_PARAMS = []  # [k for k in PARAM_FUNC().keys()][1:-3]
+
+# multi objective
+MULTI_OBJECTIVE = False
+MULTI_OBJECTIVE_DIRECTIONS = ['minimize', 'minimize']
+
+# optimization
+ITERATIONS = 1000
+OPTIMIZER = ['optuna', 'evotorch'][0]
+SAMPLER = TPESampler()  # n_startup_trials=100, n_ei_candidates=100) #optuna.samplers.CmaEsSampler()
+
+# logging
+STUDY_NAME = '-'.join(
+    [str(len(PARAM_FUNC()) - len(FIXED_PARAMS)), 'gauss', str(MAX_DEVIATION),
+     OPTIMIZER, 'v5'])
+GROUP_NAME = 'gauss'
+WANDB_ENTITY = 'hzb-aos'
+WANDB_PROJECT = 'metrix_hackathon_offsets'
+OPTUNA_STORAGE_PATH = "sqlite:////dev/shm/db.sqlite2"
+LOGGING = True
+VERBOSE = True
+
+ENGINE = GaussEngine()
