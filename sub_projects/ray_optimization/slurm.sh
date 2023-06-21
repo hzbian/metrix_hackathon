d48dd2cd8dc74695717208716c53d13eae65acec
David Meier <meier.david@uni-kassel.de>
2023-06-21 15:06:22 +0200

    Add slurm batch file

---
+++ sub_projects/ray_optimization/slurm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=bl_tpe
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task 2
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --partition=main
+#SBATCH --spread-job
+
+python3 ray_optimization.py
