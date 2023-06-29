d41589cdfbc89e33c4420dfa98c8754e7d5ef16d
David Meier <meier.david@uni-kassel.de>
2023-06-29 17:50:51 +0200

    Change ray workdir to shm

--- sub_projects/testing/ray_engine_test_v5.py
+++ sub_projects/testing/ray_engine_test_v5.py
@@ -20,7 +20,7 @@ engine = RayEngine(rml_basefile='../../rml_src/METRIX_U41_G1_H1_318eV_PS_MLearn.
                    exported_planes=exported_planes,
                    ray_backend=RayBackendDockerRAYUI(docker_image='ray-ui-service',
                                                      docker_container_name='ray-ui-service-test',
-                                                     ray_workdir='../../ray_workdir',
+                                                     ray_workdir='/dev/shm/ray_workdir',
                                                      verbose=True),
                    num_workers=-1,
                    as_generator=False)
