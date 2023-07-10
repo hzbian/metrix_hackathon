4777f424c1e7afdf649e4ad5f3817b9730a02a78
David Meier <meier.david@uni-kassel.de>
2023-07-10 14:38:26 +0200

    Hardcode ImagePlane for now since quotes do not work in cmd command for podman

--- ray_tools/base/backend.py
+++ ray_tools/base/backend.py
@@ -179,7 +179,7 @@ class RayBackendDockerRAYUI(RayBackend):
             else:
                 podman_command = f"podman run --security-opt label=disable --mount=type=bind,src={self.ray_workdir},"\
                                  f"dst={self._rayui_workdir},relabel=shared -t {self.docker_image} bash -c "
-                podman_command = f"podman exec test python3 /opt/script_rayui_bg.py {docker_rml_workfile} -p {cmd_exported_planes}"
+                podman_command = f"podman exec test python3 /opt/script_rayui_bg.py {docker_rml_workfile} -p ImagePlane"
                 check_call(podman_command.split(), stdout=DEVNULL, stderr=STDOUT)
             retry = False
             # fail indicator: any required CSV-file is missing
