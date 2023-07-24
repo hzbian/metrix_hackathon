4b187d5d68c8359c2cb9d95ad4875e2fd9786e35
David Meier <meier.david@uni-kassel.de>
2023-07-24 19:29:00 +0200

    Add test for params mse

--- sub_projects/testing/ray_optimizer_test.py
+++ sub_projects/testing/ray_optimizer_test.py
@@ -25,7 +25,7 @@ class RayOptimizerTest(unittest.TestCase):
                                                              verbose=True),
                            num_workers=-1,
                            as_generator=False)
-        #opti: RayOptimizer = RayOptimizer(optimizer_backend=OptimizerBackendOptuna(optuna_study=optuna_study), criterion=sinkhorn_loss, engine=engine, exported_plane='ImagePlane')
+        # opti: RayOptimizer = RayOptimizer(optimizer_backend=OptimizerBackendOptuna(optuna_study=optuna_study), criterion=sinkhorn_loss, engine=engine, exported_plane='ImagePlane')
 
         self.assertEqual('foo'.upper(), 'FOO')
 
@@ -40,15 +40,21 @@ class RayOptimizerTest(unittest.TestCase):
         initial_params = param_func()
         param_container_list = [initial_params]
         evaluation_parameters = RayOptimizer.get_evaluation_parameters(param_container_list, [parameters])
-        self.assertEqual(evaluation_parameters[0]['U41_318eV.translationXerror'].get_value(), initial_params['U41_318eV.translationXerror'].get_value() - 0.1)
-        
+        self.assertEqual(evaluation_parameters[0]['U41_318eV.translationXerror'].get_value(),
+                         initial_params['U41_318eV.translationXerror'].get_value() - 0.1)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_parameters_mse(self):
+        rg = RandomGenerator(seed=42)
+        param_func = lambda: RayParameterContainer([
+            ("U41_318eV.numberRays", NumericalParameter(value=1e4)),
+            ("tX", RandomParameter(value_lims=(0., 0.5), rg=rg))])
+        params_a = param_func()
+        params_b = param_func()
+        mse = RayOptimizer.parameters_mse(params_a, params_b, param_func())
+        tXa = params_a['tX']
+        tXb = params_b['tX']
+        second_mse = ((tXa.get_value() * 2) - (tXb.get_value() * 2)) ** 2
+        self.assertEqual(mse, second_mse)
 
 
 if __name__ == '__main__':
