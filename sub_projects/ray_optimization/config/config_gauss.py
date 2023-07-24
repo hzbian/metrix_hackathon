fef48949a0d5fe3a805d9ffbba07c013103f8d96
David Meier <meier.david@uni-kassel.de>
2023-07-24 19:29:40 +0200

    Fix most parameters for better testing, better naming

--- sub_projects/ray_optimization/config/config_gauss.py
+++ sub_projects/ray_optimization/config/config_gauss.py
@@ -13,7 +13,7 @@ N_RAYS = ['1e4']
 Z_LAYERS = [-15, -10, -5, 0, 5, 10, 15, 20, 25, 30]
 TRANSFORMS = MultiLayer(Z_LAYERS, copy_directions=False)
 NUM_BEAMLINE_PARAM_SAMPLES = 22
-RG = RandomGenerator(seed=42)
+RG = RandomGenerator(seed=46)
 
 PARAM_FUNC = lambda: RayParameterContainer([
     ("number_rays", NumericalParameter(value=1e2)),
@@ -21,10 +21,10 @@ PARAM_FUNC = lambda: RayParameterContainer([
     ("y_dir", NumericalParameter(value=0.)),
     ("z_dir", NumericalParameter(value=1.)),
     ("direction_spread", NumericalParameter(value=0.)),
-    ("x_mean", RandomParameter(value_lims=(-2., 2.), rg=RG)),
+    ("x_mean", NumericalParameter(value=0.)),
     ("y_mean", RandomParameter(value_lims=(-2., 2.), rg=RG)),
-    ("x_var", RandomParameter(value_lims=(0.003, 0.01), rg=RG)),
-    ("y_var", RandomParameter(value_lims=(0.003, 0.01), rg=RG)),
+    ("x_var", NumericalParameter(value=0.005)),
+    ("y_var", NumericalParameter(value=0.005)),
 ])
 
 FIXED_PARAMS = []  # [k for k in PARAM_FUNC().keys()][1:-3]
@@ -40,7 +40,7 @@ SAMPLER = TPESampler()  # n_startup_trials=100, n_ei_candidates=100) #optuna.sam
 
 # logging
 STUDY_NAME = '-'.join(
-    [str(len(PARAM_FUNC()) - len(FIXED_PARAMS)), 'gauss', str(MAX_DEVIATION),
+    [str(sum(isinstance(x, RandomParameter) for x in PARAM_FUNC().values()) - len(FIXED_PARAMS)), 'gauss', str(MAX_DEVIATION),
      OPTIMIZER, 'v5'])
 WANDB_ENTITY = 'hzb-aos'
 WANDB_PROJECT = 'metrix_hackathon_gauss'
