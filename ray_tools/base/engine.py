f43b3b4cb2c6bfb98f2590265c704f56d5398759
David Meier <meier.david@uni-kassel.de>
2023-11-08 15:21:27 +0100

    Change correlation to take rotation matrices

--- ray_tools/base/engine.py
+++ ray_tools/base/engine.py
@@ -161,14 +161,14 @@ class GaussEngine(Engine):
             y_var = param_container['y_var'].get_value()
             n_rays = int(param_container['number_rays'].get_value())
             if 'correlation_factor' in param_container:
-                correlation_factor = param_container['correlation_factor'].get_value()
+                correlation_factor = torch.tensor(param_container['correlation_factor'].get_value(), device=self.device)
             else:
                 correlation_factor = 0.
-            correlation = correlation_factor * torch.sqrt(torch.tensor(x_var, device=self.device)) * torch.sqrt(torch.tensor(y_var, device=self.device))
+
             m = torch.distributions.multivariate_normal.MultivariateNormal(torch.tensor([x_mean, y_mean], device=self.device),
-                                                                           torch.tensor([[x_var, correlation],
-                                                                                         [correlation, y_var]], device=self.device))
+                                                                           torch.diag(torch.tensor([x_var, y_var], device=self.device)))
             samples = m.rsample(torch.Size([n_rays]))
+            samples @= torch.tensor([[torch.cos(correlation_factor), -torch.sin(correlation_factor)],[torch.sin(correlation_factor), torch.cos(correlation_factor)]], device=self.device)
             samples_directions = torch.rand([n_rays, 3], device=self.device) * param_container['direction_spread'].get_value()
             ray_out = RayOutput(samples[:, 0], samples[:, 1], torch.zeros_like(samples[:, 0]),
                                 param_container['x_dir'].get_value() + samples_directions[:, 0],
