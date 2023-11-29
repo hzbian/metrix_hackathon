30446b6a0dea9416262de1773128ac7e2c119297
David Meier <meier.david@uni-kassel.de>
2023-11-29 17:37:10 +0100

    Make slurm file more sophisticated

--- slurm.sh
+++ slurm.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=bl_tpe
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task 2
+#SBATCH --cpus-per-task 25
 #SBATCH --mem-per-cpu=2GB
 #SBATCH --partition=main
 #SBATCH --spread-job
 
-python3 -m  "sub_projects.ray_optimization.ray_optimization"
+./local-venv.sh python3 -m  "sub_projects.ray_optimization.ray_optimization" target_configuration=metrixs_real target_configuration.max_target_deviation=0.3 # ray_optimizer.criterion._target_="sub_projects.ray_optimization.losses.torch.MSELoss" #+study_name.appendix="big_e2_tY+image_plane_tX" optimization_target_configuration.param_func.parameters={E2.translationYerror:"[-3.,3.]"} target_configuration.param_func.parameters={ImagePlane.translationXerror:"[-10.0,10.0]"}
