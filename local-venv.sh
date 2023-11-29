72977519f25cf7976afcc8b5a630033dcb53320f
David Meier <meier.david@uni-kassel.de>
2023-11-29 17:38:40 +0100

    Add local venv

---
+++ local-venv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export MAMBA_ROOT_PREFIX="/scratch/$USER/micromamba"
+micromamba="/mnt/work/xfel/env/micromamba-1.4.4-0/bin/micromamba"
+eval "$($micromamba shell hook --shell bash)"
+micromamba activate ray-ssd
+if [ ! $? -eq 0 ]; then
+	micromamba create -y -n ray-ssd -c conda-forge python=3.11
+	micromamba activate ray-ssd
+fi
+pip install -U torch torchvision pytorch-lightning docker hydra-core h5py ax-platform evotorch optuna plotly matplotlib scipy numpy pandas raypyng wandb
+pip install -U pykeops geomloss
+micromamba install -y -c nvidia cuda-nvrtc-dev
+"$@"
