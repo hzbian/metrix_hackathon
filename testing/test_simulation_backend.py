f3d510b86b2e3335d34e06e677781054c7a832fa
David Meier <meier.david@uni-kassel.de>
2023-12-15 18:39:59 +0100

    Add testing for simulation backend

---
+++ testing/test_simulation_backend.py
@@ -0,0 +1,28 @@
+import unittest
+
+import hydra
+import omegaconf
+from hydra.utils import instantiate
+from hydra import initialize, compose
+from ray_optim.ray_optimizer import OffsetTarget, RayOptimizer
+
+from ray_tools.base.backend import RayBackendDockerRAYUI
+from ray_tools.base.engine import RayEngine
+from ray_tools.base.parameter import NumericalParameter, RandomParameter, RayParameterContainer
+from ray_tools.base.transform import Histogram, RayTransformConcat, MultiLayer
+from ray_tools.base.utils import RandomGenerator
+from sub_projects.ray_optimization.ray_optimization import RayOptimization
+
+
+class SimulationBackendTest(unittest.TestCase):
+    def test_with_initialize(self) -> None:
+            with initialize(version_base=None, config_path="../sub_projects/ray_optimization/conf"):
+                # config is relative to a module
+                cfg = compose(config_name="config", overrides=["logging_backend=",
+                "+logging_backend._target_=ray_optim.logging.DebugPlotBackend", "target_configuration=metrixs", "target_configuration.max_target_deviation=0.02"],)
+                print(omegaconf.OmegaConf.to_yaml(cfg))
+                ray_optimization: RayOptimization = instantiate(cfg)
+                ray_optimization.setup_target()
+                target: OffsetTarget = ray_optimization.target
+                out_dict = RayOptimizer.plot_initial_plots(target, "ImagePlane")
+                out_dict['fancy_footprint'].write_html('out.html')
