76b685fb8bf48308268421a5efbbe0922eaf23a7
David Meier <meier.david@uni-kassel.de>
2023-12-19 02:44:01 +0100

    Reintroduce support for ax

--- ray_optim/ax.py
+++ ray_optim/ax.py
@@ -1,7 +1,7 @@
 # this needs some major work
 import time
-from typing import List
-
+from typing import List, Optional
+from ax.service.ax_client import AxClient, ObjectiveProperties
 from ax.service.ax_client import AxClient
 from tqdm import trange
 from ray_optim.ray_optimizer import OptimizerBackend, Target
@@ -9,24 +9,26 @@ from ray_tools.base.parameter import MutableParameter, NumericalParameter, RayPa
 
 
 class OptimizerBackendAx(OptimizerBackend):
-   def __init__(self, ax_client: AxClient, search_space: RayParameterContainer):
-       self.search_space = search_space
-       self.ax_client = ax_client
+   def __init__(self, ax_client: Optional[AxClient] = None):
+       if ax_client is not None:
+           self.ax_client = ax_client
+       else:
+           self.ax_client = AxClient()
 
-   def optimizer_parameter_to_container_list(self, optimizer_parameter) -> List[RayParameterContainer]:
-       trial_params_first_key = min(optimizer_parameter.keys())
-       param_container_list = []
-
-       for i in range(trial_params_first_key, trial_params_first_key + len(optimizer_parameter)):
-           all_parameters_copy = self.search_space.copy()
-           for param_key in optimizer_parameter[trial_params_first_key].keys():
-               all_parameters_copy.__setitem__(param_key, NumericalParameter(optimizer_parameter[i][param_key]))
+   def optimizer_parameter_to_container_list(self, optimizer_parameter, search_space) -> tuple[list[int], list[RayParameterContainer]]:
+       trial_index_list: list[int] = []
+       param_container_list: list[RayParameterContainer] = []
+       for trial_index, param_container in optimizer_parameter.items():
+           all_parameters_copy = search_space.copy()
+           for param_key in param_container.keys():
+               all_parameters_copy.__setitem__(param_key, NumericalParameter(param_container[param_key]))
            param_container_list.append(all_parameters_copy)
-       return param_container_list
-
-   def setup_optimization(self):
+           trial_index_list.append(trial_index)
+       return trial_index_list, param_container_list
+   
+   def setup_optimization(self, target: Target):
        experiment_parameters = []
-       for key, value in self.search_space.items():
+       for key, value in target.search_space.items():
            if isinstance(value, MutableParameter):
                experiment_parameters.append(
                    {"name": key, "type": "range", 'value_type': 'float', "bounds": list(value.value_lims)})
@@ -34,8 +36,7 @@ class OptimizerBackendAx(OptimizerBackend):
        self.ax_client.create_experiment(
            name="metrix_experiment",
            parameters=experiment_parameters,
-           objective_name="metrix",
-           minimize=True,
+           objectives={"mse": ObjectiveProperties(minimize=True)},
        )
 
    def optimize(self, objective, iterations, target: Target):
@@ -45,12 +46,11 @@ class OptimizerBackendAx(OptimizerBackend):
            trials_to_evaluate = self.ax_client.get_next_trials(max_trials=10)[0]
            print("Optimization took {:.2f}s".format(time.time() - optimization_time))
 
-           ray_parameter_container_list = self.optimizer_parameter_to_container_list(trials_to_evaluate)
-           results = objective(ray_parameter_container_list, target.target_rays,
-                               target_params=target.target_params)
+           trial_index_list, ray_parameter_container_list = self.optimizer_parameter_to_container_list(trials_to_evaluate, target.search_space)
+           results = objective(ray_parameter_container_list, target=target)
 
-           for trial_index in results:
-               self.ax_client.complete_trial(trial_index, results[trial_index])
+           for i in range(len(results)):
+               self.ax_client.complete_trial(trial_index_list[i], {'mse': results[i].item()})
 
        best_parameters, metrics = self.ax_client.get_best_parameters()
        return best_parameters, metrics
