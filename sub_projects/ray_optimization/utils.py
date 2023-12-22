c82bc04545d4ba8276c0f946ffc8dac7a0710473
David Meier <meier.david@uni-kassel.de>
2023-12-22 19:07:22 +0100

    Add dim argument for type checking

--- sub_projects/ray_optimization/utils.py
+++ sub_projects/ray_optimization/utils.py
@@ -2,7 +2,7 @@ from typing import Union, List, Dict, Iterable
 import torch
 torch.set_num_threads(1)
 
-def ray_output_to_tensor(ray_output: Union[Dict, List[Dict], Iterable[Dict]], exported_plane: str, to_cpu=False):
+def ray_output_to_tensor(ray_output: Union[Dict, List[Dict], Iterable[Dict]], exported_plane: str, to_cpu=False) -> torch.Tensor | list[torch.Tensor]:
     if not isinstance(ray_output, Dict):
         return [ray_output_to_tensor(element, exported_plane) for element in
                 ray_output]
@@ -10,7 +10,7 @@ def ray_output_to_tensor(ray_output: Union[Dict, List[Dict], Iterable[Dict]], ex
         rays: dict = ray_output['ray_output'][exported_plane]
         x_locs = torch.stack([value.x_loc.clone().detach() for value in rays.values()])
         y_locs = torch.stack([value.y_loc.clone().detach() for value in rays.values()])
-        output = torch.stack((x_locs, y_locs), -1)
+        output = torch.stack((x_locs, y_locs), dim=-1)
         if to_cpu:
             return output.cpu()
         else:
