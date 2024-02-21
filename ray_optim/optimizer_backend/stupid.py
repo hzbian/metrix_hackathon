de62014ce46a0bdd054273394305c2e05fe0c8de
David Meier <meier.david@uni-kassel.de>
2024-02-21 20:23:11 +0100

    Add cosine annealing

--- ray_optim/optimizer_backend/stupid.py
+++ ray_optim/optimizer_backend/stupid.py
@@ -2,7 +2,7 @@
 from collections.abc import Callable
 from typing import Any
 import random
-
+import math
 from tqdm import tqdm
 from ray_optim.optimizer_backend.base import OptimizerBackend
 from ray_optim.target import Target
@@ -10,8 +10,9 @@ from ray_tools.base.parameter import MutableParameter, NumericalParameter, RayPa
 
 
 class OptimizerBackendStupid(OptimizerBackend):
-    def __init__(self, step_size: float = 0.1):
+    def __init__(self, step_size: float = 0.1, cosine_annealing: bool = True):
         self.step_size = step_size
+        self.cosine_annealing: bool = cosine_annealing
     def optimize(self, objective: Callable[[list[RayParameterContainer], Target], list[float]], iterations: int, target: Target, starting_point: dict[str, float] | None = None) -> tuple[dict[str, float], dict[str, float]]:
         optimize_parameters: RayParameterContainer = target.search_space.clone()
         if starting_point is not None:
@@ -25,13 +26,15 @@ class OptimizerBackendStupid(OptimizerBackend):
         all_time_best_parameters: RayParameterContainer = current_parameters
         current_best_loss: float = all_time_best_loss
         q = tqdm(range(iterations))
-        for _ in q:
+        for i in q:
             q.set_postfix({"best_loss": all_time_best_loss, "cur_loss": current_best_loss})
             perturbation_key = random.choice(mutable_parameters_keys)
             perturbation_sign = 1 if random.random() < 0.5 else -1
             perturbation_parameters = current_parameters[perturbation_key]
             assert isinstance(perturbation_parameters, MutableParameter)
-            perturbation_value: float = perturbation_sign * (perturbation_parameters.value_lims[1]-perturbation_parameters.value_lims[0]) / 2 * self.step_size
+            if self.cosine_annealing:
+                annealing_factor = math.cos(i*iterations)
+            perturbation_value: float = perturbation_sign * (perturbation_parameters.value_lims[1]-perturbation_parameters.value_lims[0]) / 2 * self.step_size * annealing_factor
             current_parameters_copy: RayParameterContainer = current_parameters.clone()
             current_parameters_copy.perturb(RayParameterContainer({perturbation_key: NumericalParameter(perturbation_value)}))
             losses: list[float] = objective([current_parameters_copy], target)
