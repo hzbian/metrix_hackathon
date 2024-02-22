66a70cbcd05b54d600b043d9cf3c620de7079d34
David Meier <meier.david@uni-kassel.de>
2024-02-22 14:40:20 +0100

    Improve annealing

--- ray_optim/optimizer_backend/stupid.py
+++ ray_optim/optimizer_backend/stupid.py
@@ -2,7 +2,6 @@
 from collections.abc import Callable
 from typing import Any
 import random
-import math
 from tqdm import tqdm
 from ray_optim.optimizer_backend.base import OptimizerBackend
 from ray_optim.target import Target
@@ -10,9 +9,9 @@ from ray_tools.base.parameter import MutableParameter, NumericalParameter, RayPa
 
 
 class OptimizerBackendStupid(OptimizerBackend):
-    def __init__(self, step_size: float = 0.1, cosine_annealing: bool = True):
+    def __init__(self, step_size: float = 0.1, annealing: bool = True):
         self.step_size = step_size
-        self.cosine_annealing: bool = cosine_annealing
+        self.annealing: bool = annealing
     def optimize(self, objective: Callable[[list[RayParameterContainer], Target], list[float]], iterations: int, target: Target, starting_point: dict[str, float] | None = None) -> tuple[dict[str, float], dict[str, float]]:
         optimize_parameters: RayParameterContainer = target.search_space.clone()
         if starting_point is not None:
@@ -32,8 +31,10 @@ class OptimizerBackendStupid(OptimizerBackend):
             perturbation_sign = 1 if random.random() < 0.5 else -1
             perturbation_parameters = current_parameters[perturbation_key]
             assert isinstance(perturbation_parameters, MutableParameter)
-            if self.cosine_annealing:
-                annealing_factor = math.cos(i*iterations)
+            if self.annealing:
+                annealing_factor = 1./i
+            else:
+                annealing_factor = 1.
             perturbation_value: float = perturbation_sign * (perturbation_parameters.value_lims[1]-perturbation_parameters.value_lims[0]) / 2 * self.step_size * annealing_factor
             current_parameters_copy: RayParameterContainer = current_parameters.clone()
             current_parameters_copy.perturb(RayParameterContainer({perturbation_key: NumericalParameter(perturbation_value)}))
