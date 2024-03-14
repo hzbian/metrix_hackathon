72513648018a8cac9e649fbbd6d6ed4545152007
David Meier <meier.david@uni-kassel.de>
2024-03-14 20:32:56 +0100

    Adapt ray workdir

--- datasets/metrix_simulation/config_ray_emergency_surrogate.py
+++ datasets/metrix_simulation/config_ray_emergency_surrogate.py
@@ -11,7 +11,7 @@ H5_IDX_RANGE = range(100)
 H5_DATADIR = os.path.join(ROOT_DIR, 'datasets', 'metrix_simulation', DATASET_NAME)
 
 RML_BASEFILE = os.path.join(ROOT_DIR, 'rml_src', 'METRIX_U41_G1_H1_318eV_PS_MLearn.rml')
-RAY_WORKDIR = os.path.join(ROOT_DIR, 'ray_workdir', DATASET_NAME)
+RAY_WORKDIR = '/dev/shm/ray_workdir' 
 
 N_RAYS = ['1e5']
 
