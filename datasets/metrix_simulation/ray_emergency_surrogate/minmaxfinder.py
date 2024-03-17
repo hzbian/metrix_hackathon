ad9001b5a7b7998d125e6d3def5177fd3f7f06a5
David Meier <meier.david@uni-kassel.de>
2024-03-17 13:35:50 +0100

    Add minmaxfinder

---
+++ datasets/metrix_simulation/ray_emergency_surrogate/minmaxfinder.py
@@ -0,0 +1,31 @@
+import h5py
+import glob
+from tqdm import tqdm
+
+if __name__ == '__main__':
+    file_list = list(glob.iglob('data_raw_*.h5'))
+    x_min = float('inf')
+    y_min = float('inf')
+    x_max = -float('inf')
+    y_max = -float('inf')
+    for i, file in tqdm(enumerate(file_list)):
+        with h5py.File(file, 'r') as f:
+            for key in f.keys():
+                for subkey in f[key].keys():                    
+                    input_hist = f[key][subkey]['ray_output']['ImagePlane']['hist']['histogram'][:]
+                    x_lims = f[key][subkey]['ray_output']['ImagePlane']['hist']['x_lims'][:]
+                    y_lims = f[key][subkey]['ray_output']['ImagePlane']['hist']['y_lims'][:]
+                    num_rays = f[key][subkey]['ray_output']['ImagePlane']['hist']['n_rays'][()]
+                    if num_rays > 0:
+                        if x_lims[0] < x_min:
+                            x_min = x_lims[0]
+                        if x_lims[1] > x_max:
+                            x_max = x_lims[1]
+                        if y_lims[0] < y_min:
+                            y_min = y_lims[0]
+                        if y_lims[1] > y_max:
+                            y_max = y_lims[1]
+                        
+
+    print("x", str(x_min), str(x_max))
+    print("y", str(y_min), str(y_max))
