ae728dc7fb9e9850248ce7ee53417f10ff4e6df9
David Meier <meier.david@uni-kassel.de>
2024-05-14 19:25:54 +0200

    Add forward and hist surrogate engine

--- ray_nn/nn/xy_hist_data_models.py
+++ ray_nn/nn/xy_hist_data_models.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 import glob
 import math
 import torch
@@ -12,6 +13,9 @@ import wandb
 
 from datasets.metrix_simulation.config_ray_emergency_surrogate import PARAM_CONTAINER_FUNC as params
 from ray_nn.data.lightning_data_module import DefaultDataModule
+from ray_tools.base.engine import Engine
+from ray_tools.base.parameter import RayParameterContainer
+from ray_tools.base.transform import RayTransform
 from ray_tools.simulation.torch_datasets import BalancedMemoryDataset, RayDataset
 from ray_nn.data.transform import Select
 
@@ -33,6 +37,9 @@ class MetrixXYHistSurrogate(L.LightningModule):
         self.register_buffer("train_y_hat_empty_plot_data", torch.tensor([]))
         self.register_buffer("train_y_hat_plot_data", torch.tensor([]))
         print(self.net)
+
+    def forward(self, input):
+        return self.net(input)
 
     def create_sequential(self, input_length, output_length, layer_size, blow: int | float =0, shrink_factor="log", activation_function: Module=nn.ReLU(), last_activation: Module | None = None):
         layers = [input_length]
@@ -167,31 +174,47 @@ class MetrixXYHistSurrogate(L.LightningModule):
 class StandardizeXYHist(torch.nn.Module):
     def forward(self, element):
         return element / 2500.
+    
+if __name__ == '__main__':
+    load_len: int | None = None
+    dataset_normalize_outputs = True
+    h5_files = list(glob.iglob('datasets/metrix_simulation/ray_emergency_surrogate/50+50_data_raw_*.h5')) # ['datasets/metrix_simulation/ray_emergency_surrogate/49+50_data_raw_0.h5']
+    dataset = RayDataset(h5_files=h5_files,
+                        sub_groups=['1e5/params',
+                                    '1e5/histogram', '1e5/n_rays'], transform=Select(keys=['1e5/params', '1e5/histogram', '1e5/n_rays'], search_space=params(), non_dict_transform={'1e5/histogram': StandardizeXYHist()}))
 
-load_len: int | None = None
-dataset_normalize_outputs = True
-h5_files = list(glob.iglob('datasets/metrix_simulation/ray_emergency_surrogate/50+50_data_raw_*.h5')) # ['datasets/metrix_simulation/ray_emergency_surrogate/49+50_data_raw_0.h5']
-dataset = RayDataset(h5_files=h5_files,
-                     sub_groups=['1e5/params',
-                                 '1e5/histogram', '1e5/n_rays'], transform=Select(keys=['1e5/params', '1e5/histogram', '1e5/n_rays'], search_space=params(), non_dict_transform={'1e5/histogram': StandardizeXYHist()}))
+    memory_dataset = BalancedMemoryDataset(dataset=dataset, load_len=load_len, min_n_rays=500)
+    datamodule = DefaultDataModule(dataset=memory_dataset, num_workers=4)
+    datamodule.prepare_data()
+    model = MetrixXYHistSurrogate(dataset_length=load_len, dataset_normalize_outputs=dataset_normalize_outputs)
+    test = False
+    wandb_logger = WandbLogger(name="ref_bal_500_sch_.999", project="xy_hist", save_dir='outputs')
+    #wandb_logger = None
+    if test:
+        datamodule.setup(stage="test")
+    else:
+        datamodule.setup(stage="fit")
 
-memory_dataset = BalancedMemoryDataset(dataset=dataset, load_len=load_len, min_n_rays=500)
-datamodule = DefaultDataModule(dataset=memory_dataset, num_workers=4)
-datamodule.prepare_data()
-model = MetrixXYHistSurrogate(dataset_length=load_len, dataset_normalize_outputs=dataset_normalize_outputs)
-test = False
-wandb_logger = WandbLogger(name="ref_bal_500_sch_.999", project="xy_hist", save_dir='outputs')
-#wandb_logger = None
-if test:
-    datamodule.setup(stage="test")
-else:
-    datamodule.setup(stage="fit")
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+    trainer = L.Trainer(max_epochs=10000, logger=wandb_logger, log_every_n_steps=100, check_val_every_n_epoch=1, callbacks=[lr_monitor])
+    trainer.init_module()
 
-lr_monitor = LearningRateMonitor(logging_interval='step')
-trainer = L.Trainer(max_epochs=10000, logger=wandb_logger, log_every_n_steps=100, check_val_every_n_epoch=1, callbacks=[lr_monitor])
-trainer.init_module()
+    if test:
+        trainer.test(datamodule=datamodule, ckpt_path='outputs/xy_hist/on2yv96j/checkpoints/epoch=999-step=25000000.ckpt', model=model)
+    else:
+        trainer.fit(model=model, datamodule=datamodule)
 
-if test:
-    trainer.test(datamodule=datamodule, ckpt_path='outputs/xy_hist/on2yv96j/checkpoints/epoch=999-step=25000000.ckpt', model=model)
-else:
-    trainer.fit(model=model, datamodule=datamodule)
+class HistSurrogateEngine(Engine):
+    def __init__(self, module, checkpoint_path: str="outputs/xy_hist/muqyzwbp/epoch=8739-step=106732880.ckpt"):
+        super().__init__()
+        self.model = module.load_from_checkpoint(checkpoint_path)
+        self.model.eval()
+        self.select = Select(keys=['1e5/params'], mutable_only=True, non_dict_transform={'1e5/histogram': StandardizeXYHist()})
+
+    def run(self, param_containers: list[RayParameterContainer], transforms: RayTransform | dict[str, RayTransform] | Iterable[RayTransform | dict[str, RayTransform]] | None = None) -> list[dict]:
+        param_containers_tensor = torch.vstack([self.select({"1e5/params":param_container})[0] for param_container in param_containers])
+        with torch.no_grad():
+            output = self.model(param_containers_tensor)
+            hist_len = int(output.shape[-1] / 2)
+
+            return [{'x_hist': output_element[:hist_len], 'y_hist': output_element[output.shape[-1]-hist_len:]} for output_element in output]
