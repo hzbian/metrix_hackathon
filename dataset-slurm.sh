d251e31dd2a8818a3bc1d8c059eb4a978ed667e7
David Meier <meier.david@uni-kassel.de>
2024-07-11 20:12:43 +0200

    Add dataset spread generation

--- dataset-slurm.sh
+++ dataset-slurm.sh
@@ -2,9 +2,10 @@
 #SBATCH --job-name=dataset
 #SBATCH --gres=gpu:0
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task 125
+#SBATCH --array=1-11
+#SBATCH --cpus-per-task 100
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --partition=main
 #SBATCH --spread-job
 
-        ./local-venv.sh python3 -m datasets.metrix_simulation.ray_emergency_surrogate.rebin #datasets.dataset_tools.script_generate_data
+        ./local-venv.sh python3 -m datasets.dataset_tools.script_generate_data
