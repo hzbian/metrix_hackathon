2b27d79a60c2e78eb9161f58311deb90d5e8d1e3
David Meier <meier.david@uni-kassel.de>
2024-07-29 20:01:10 +0200

    Improve ray counter

--- ray_nn/nn/ray_count.py
+++ ray_nn/nn/ray_count.py
@@ -4,17 +4,16 @@ import glob
 from ray_nn.data.transform import Select
 from ray_tools.simulation.torch_datasets import MemoryDataset, RayDataset
 
-load_len: int | None =  100
-dataset_normalize_outputs = True
-h5_files = list(glob.iglob('datasets/metrix_simulation/ray_emergency_surrogate/50+50_data_raw_*.h5')) # ['datasets/metrix_simulation/ray_emergency_surrogate/49+50_data_raw_0.h5']
+load_len: int | None = None
+h5_files = list(glob.iglob('datasets/metrix_simulation/ray_emergency_surrogate/data_raw_*.h5'))
 dataset = RayDataset(h5_files=h5_files,
-                     sub_groups=['1e5/params',
-                                 '1e5/n_rays'], transform=Select(keys=['1e5/params', '1e5/n_rays']))
+                        sub_groups=['1e5/params',
+                                    '1e5/ray_output/ImagePlane/histogram', '1e5/ray_output/ImagePlane/n_rays'], transform=Select(keys=['1e5/params', '1e5/ray_output/ImagePlane/histogram', '1e5/ray_output/ImagePlane/n_rays']))
 
 memory_dataset = MemoryDataset(dataset=dataset, load_len=load_len)
 new_min = float('inf')
 new_max = float('-inf')
-for x,y in memory_dataset:
-    new_min = min(new_min, y)
-    new_max = max(new_max, y)
+for params,hist,n_rays in memory_dataset:
+    new_min = min(new_min, hist.min())
+    new_max = max(new_max, hist.max())
 print(new_min, new_max)
