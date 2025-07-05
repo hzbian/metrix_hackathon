ffc4f65a436e5737b459c34441fa458c7251e83b
David Meier <meier.david@uni-kassel.de>
2025-07-05 04:09:12 +0200

    Add seaborn since required for some plots in surrogate model

--- local-venv.sh
+++ local-venv.sh
@@ -8,7 +8,7 @@ if [ ! $? -eq 0 ]; then
 	micromamba create -y -n ray-ssd -c conda-forge python=3.11
 	micromamba activate ray-ssd
 fi
-pip install -U torch torchvision pytorch-lightning lightning docker hydra-core h5py ax-platform evotorch optuna plotly matplotlib scipy numpy pandas raypyng wandb blop
+pip install -U torch torchvision pytorch-lightning lightning docker hydra-core h5py ax-platform evotorch optuna plotly matplotlib scipy numpy pandas raypyng wandb blop seaborn
 pip install -U pykeops geomloss
 micromamba install -y -c nvidia cuda-nvrtc-dev
 "$@"
